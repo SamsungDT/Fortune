35e87c5285e800ef30f863025a9ae8c04b9c26fa
kdongsu5509 <kod66170@gmail.com>
2025-09-29 12:02:32 +0900

    feat 사용자 횟수 랜덤으로 변경

--- Backend/src/test/java/dsko/hier/security/infra/UserRepositoryImplTest.java
+++ Backend/src/test/java/dsko/hier/security/infra/UserRepositoryImplTest.java
@@ -2,10 +2,14 @@ package dsko.hier.security.infra;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import dsko.hier.security.domain.User;
 import dsko.hier.security.domain.UserRole;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +18,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 @ExtendWith(MockitoExtension.class)
 class UserRepositoryImplTest {
@@ -97,5 +104,47 @@ class UserRepositoryImplTest {
 
         // Then
         assertThat(foundUser).isNotPresent();
+    }
+
+    @Test
+    @DisplayName("전체 사용자 조회")
+    void findAll_success() {
+        // Given
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            User tempUser = User.builder()
+                    .email("test" + i + "@example.com")
+                    .nickname("testuser")
+                    .role(UserRole.USER)
+                    .build();
+            users.add(tempUser);
+        }
+        Pageable pageable = Pageable.ofSize(10);
+        Page<User> userPage = new PageImpl<>(users, pageable, users.size());
+
+        // When
+        when(userJpaRepository.findAll(pageable)).thenReturn(userPage);
+        Page<User> result = userRepository.findAll(pageable);
+
+        // Then
+        assertThat(result.getTotalElements()).isEqualTo(5);
+        assertThat(result.getContent()).hasSize(5);
+    }
+
+    @Test
+    @DisplayName("사용자가 존재하면 삭제가 성공한다.")
+    void delete_user_success() {
+        // Given
+        User user = User.builder()
+                .email("test@example.com")
+                .nickname("testuser")
+                .role(UserRole.USER)
+                .build();
+
+        // When, Then
+        userRepository.delete(user);
+
+        //then
+        verify(userJpaRepository, times(1)).delete(user);
     }
 }
