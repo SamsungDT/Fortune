dcd6ebfd4c261260d19737ec98a4a8645ae2fc73
kdongsu5509 <kod66170@gmail.com>
2025-09-30 21:48:41 +0900

    add "https://fortuneki.site" to avoid cors

--- Backend/src/main/java/dsko/hier/security/config/SecurityConfig.java
+++ Backend/src/main/java/dsko/hier/security/config/SecurityConfig.java
@@ -98,7 +98,8 @@ public class SecurityConfig {
                 "http://127.0.0.1:5500",
                 "http://127.0.0.1:63342",
                 "http://localhost:8080",
-                "https://www.fortuneki.site"
+                "https://www.fortuneki.site",
+                "https://fortuneki.site"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Collections.singletonList("*"));
